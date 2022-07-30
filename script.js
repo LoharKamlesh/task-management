7d8cc297c431ebe22c2b5ac8f2283fb2e12d9c00
LoharKamlesh <110237836+LoharKamlesh@users.noreply.github.com>
2022-07-30 11:48:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,14 @@
 let containerMovementsNew = document.querySelector(".movements");
 const add = document.querySelector(".add__task");
 const checkBox = document.querySelector(".hide__tasks");
+const delBtn = document.getElementById("btn");
 
-let movements = ["homework", "gardening", "to-do"];
+let movements = [
+  "Clean the garden",
+  "vacuum clean the house",
+  "Buy grocerries",
+  "Sleep",
+];
 
 const displayMovements = function (movements) {
   containerMovementsNew.innerHTML = " ";
@@ -14,14 +20,27 @@ const displayMovements = function (movements) {
   movs.forEach(function (el) {
     const htmlNew = `
     
-        <div class="movements__row">
+        <div class="movements__row"><img src="list.png" alt="list" class="list" />
         ${el}
         
-            <button class="form__btn" onclick="deleteTask('${el}')">delete</button> 
+        <button class="form__btn" id="${el}"  onmouseover="mouseHover('${el}')" 
+        onmouseout="mouseOut('${el}')"
+        onclick="deleteTask('${el}')"><img src="trash-1.png" alt="trash" class="trash" />delete</button>  
     
         </div> 
+
          
      `;
+
+    //  <div class="movements__row"><img src="list.png" alt="list" class="list" />
+    //  ${el}
+
+    //  <button class="form__btn"   onmouseover="mouseHover('${el}')"
+    //  onmouseout="mouseOut('${el}')"
+
+    //  onclick="deleteTask('${el}')"><img src="trash.png" alt="trash" class="trash" />delete</button>
+
+    //  </div>
     containerMovementsNew.insertAdjacentHTML("beforeend", htmlNew);
   });
 };
@@ -29,12 +48,12 @@ const displayMovements = function (movements) {
 // displayMovements('')
 
 const addTask = function () {
-  let taskInput = document.querySelector("#addTask");
-  var taskInputVal = taskInput.value;
+  const taskInput = document.querySelector("#addTask");
+  const taskInputVal = taskInput.value;
   movements.push(taskInputVal);
   taskInput.value = "";
 
-  console.log(movements);
+  //   console.log(movements);
   displayMovements(movements);
 };
 add.addEventListener("click", addTask);
@@ -50,12 +69,34 @@ checkBox.addEventListener("change", function (e) {
 
 const deleteTask = function (arg) {
   movements = movements.filter((data) => data !== arg);
-  //   console.log(data);
+
   displayMovements(movements);
 };
 
 const searchTask = function (arg) {
-  let movementsNew = movements.filter((data) => data.includes(arg));
-  console.log(movements);
+  console.log(arg);
+  const args = arg.toLowerCase();
+  let movementsNew = movements.filter(function (data) {
+    return data.includes(args);
+  });
+
   displayMovements(movementsNew);
+};
+const mouseHover = function (el) {
+  //console.log(el);
+  const ele = document.getElementById(el);
+  //console.log(ele);
+  ele.style.backgroundColor = "green";
+  ele.style.color = "white";
+
+  // //console.log("I was hovered in");
+};
+const mouseOut = function (el) {
+  //console.log(el);
+  const ele = document.getElementById(el);
+  //console.log(ele);
+  ele.style.backgroundColor = "white";
+  ele.style.color = "black";
+
+  // //console.log("I was hovered out");
 };
